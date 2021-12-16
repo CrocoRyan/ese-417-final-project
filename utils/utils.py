e4ca6effc8f525b9a141442009d405ff53cbfa29
CrocoRyan <ryan_gg@hotmail.com>
2021-12-15 20:08:35 -0600

    update feature filter for candidate data

--- utils/utils.py
+++ utils/utils.py
@@ -1,21 +1,22 @@
 import numpy as np
+from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 import os
 
-def evaluate_by_accuracy(model, test_features, test_labels):
-    predictions = model.predict(test_features)
-    errors = abs(predictions - test_labels)
-    mape = 100 * np.mean(errors / test_labels)
-    accuracy = 100 - mape
-    print('Model Performance')
-    print('Average Error: {:0.4f} degrees.'.format(np.mean(errors)))
-    print('Accuracy = {:0.2f}%.'.format(accuracy))
-    return accuracy
+from sklearn.preprocessing import StandardScaler
 
-def load_data():
+
+def load_data(filter_column=(5,2,3,8,0)):
     # load samples
+    pca = PCA(n_components=3)
+
     X_test = np.load('..\\data\\preprocessed\\X_test.npy')
     X_train = np.load('..\\data\\preprocessed\\X_train.npy')
+    # for potential pca usage
+    # X_test = pca.fit_transform(X_test)
+    # X_train=pca.fit_transform(X_train)
+    X_test=np.delete(X_test,filter_column,axis=1)
+    X_train=np.delete(X_train,filter_column,axis=1)
     y_test = np.load('..\\data\\preprocessed\\y_test.npy')
     y_train = np.load('..\\data\\preprocessed\\y_train.npy')
     return X_train, X_test, y_train, y_test
