18f45b734ad739503a10355bf537f21febefa44f
CrocoRyan <ryan_gg@hotmail.com>
2021-12-18 21:20:27 -0600

    models and scores

--- models/random-forest.py
+++ models/random-forest.py
@@ -14,19 +14,35 @@ print('Parameters currently in use:\n')
 pprint(random_forest.get_params())
 
 performance_score_by_filter=[]
+max_accu=0
 for stop_index in range(11):
+
+
     X_train, X_test, y_train, y_test = load_data(stop_index)
+
+    # # feature selection
+    # base_model = RandomForestClassifier(n_estimators=1000)
+    # base_model.fit(X_train, y_train)
+    # mse_base = mean_squared_error(base_model.predict(X_test), y_test)
+    # print("*****base line*****")
+    # print("base model accu %d" % base_model.score(X_test, y_test))
+    # print("**********")
+    # joblib.dump(base_model, './backup/random-forest_%d.pkl' % (stop_index), compress=1)
+    #
+    # performance_score_by_filter.append( base_model.score(X_test, y_test))
+
+
     # Number of trees in random forest
-    n_estimators = [int(x) for x in np.linspace(start=200, stop=10000, num=20)]
+    n_estimators = [int(x) for x in np.linspace(start=200, stop=1000, num=20)]
     # Number of features to consider at every split
     max_features = ['auto', 'sqrt']
     # Maximum number of levels in tree
     max_depth = [int(x) for x in np.linspace(1, 12, num=11)]
     max_depth.append(None)
     # Minimum number of samples required to split a node
-    min_samples_split = [0.1, 0.2, 0.3, 0.5, 0.7]
+    min_samples_split = [2,4,8,16]
     # Minimum number of samples required at each leaf node
-    min_samples_leaf = [50, 75, 150]
+    min_samples_leaf = [1,5,10,50, 75, 150]
     # Method of selecting samples for training each tree
     bootstrap = [True, False]
     # Create the random grid
@@ -48,38 +64,14 @@ for stop_index in range(11):
 
     rf_random.fit(X_train, y_train)
 
-    base_model = RandomForestClassifier(n_estimators=200)
-    base_model.fit(X_train, y_train)
-    mse_base = mean_squared_error(base_model.predict(X_test), y_test)
-    # print("baseline score :%f" % accuracy_score(y_test, base_model.predict(X_test)))
-    print("*****base line*****")
-    print("baseline mse :%f" % mean_squared_error(y_test, base_model.predict(X_test)))
-    print("**********")
-
-    best_random = rf_random.best_estimator_
-    mse_best = mean_squared_error(best_random.predict(X_test), y_test)
-    print("best mse :%f" % mse_best)
     test_accuracy_score = rf_random.best_estimator_.score(X_test, y_test)
-    print("best accuracy score: %f" % (test_accuracy_score))
-    print("accuracy_score on training set:%f" % (accuracy_score(y_train, rf_random.best_estimator_.predict(X_train))))
+    if test_accuracy_score>max_accu:
+        joblib.dump(rf_random.best_estimator_, 'random-forest_%d.pkl' % (stop_index), compress=1)
+        print("best accuracy score: %f" % (test_accuracy_score))
+        max_accu=test_accuracy_score
+        print("accuracy_score on training set:%f" % (test_accuracy_score))
 
-    performance_score_by_filter.append(test_accuracy_score)
 
-    joblib.dump(rf_random.best_estimator_, 'random-forest_%d_%d.pkl' % (int(test_accuracy_score * 100),stop_index), compress=1)
-np.save('random-forest_performance_scores.npy', performance_score_by_filter)
+# # dump feature selection result
+# np.save('backup/random-forest_performance_score.npy', performance_score_by_filter)
 
-    # # grid search
-    # param_grid = {"max_depth": max_depth,
-    #               "n_estimators": [int(x) for x in np.linspace(start=100, stop=2000, num=10)],
-    #               "max_features": [1, 3, 10],
-    #               "min_samples_split": [3,10],
-    #               "min_samples_leaf": [3,7],
-    #               "bootstrap": [True, False],
-    #               "criterion": ["gini", "entropy"]}
-    #
-    # # grid_search= RandomForestClassifier(n_estimators=10)
-    # grid_search = GridSearchCV(random_forest, param_grid=param_grid, verbose=10)
-    # grid_search.fit(X_train, y_train)
-    # grid_accuracy = evaluate_by_accuracy(grid_search.best_estimator_, X_test, y_test)
-    # print("accuracy score of random forest model with optimal parameters: %f" % grid_accuracy)
-    # joblib.dump(grid_search.best_estimator_, 'best_xgboost_rs.pkl', compress=1)
